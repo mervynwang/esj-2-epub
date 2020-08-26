adf80463945ecfa7954bdff9d9789d915f11b315
mervyn <mervyn_wang@jswpac.com>
2020-08-26 10:15:00 +0800

    add filter for title & content

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const fetch = require('node-fetch'),
 	Epub = require("epub-gen"),
-	// OpenCC = require('opencc'),
+	OpenCC = require('opencc'),
 	fs = require("fs"),
 	cheerio = require("cheerio"),
 	sleep = require('sleep'),
 	md5 = require('md5'),
 	argv = require('argv');
-	
+
 
 var args = argv.option([
     {
@@ -18,28 +18,34 @@ var args = argv.option([
     },
     {
         name: 'epub',
-        short: 'f',
+        short: 'e',
         type: 'path',
         description:'output epub file name'
+    },
+    {
+        name: 'filter',
+        short: 'f',
+        type: 'path',
+        description:'filter reg replace, json : {title:[{regexp, replace}], content:[]}'
     },
     {
         name: 'imgs',
         short: 'i',
         type: 'list,path',
         description:'add image into epub'
-    },    
+    },
     {
         name: 'trad',
         short: 't',
         type: 'bool',
         description:'Traditional'
-    },    
+    },
     {
         name: 'debug',
         short: 'd',
         type: 'bool',
         description:'Debug Info'
-    },    
+    },
     {
         name: 'nu',
         short: 'n',
@@ -57,6 +63,13 @@ const tw = args.options.trad? true: false,
 	  debug = args.options.debug? true: false;
 const converter = tw? new OpenCC('s2tw.json') : false;
 
+var filter;
+if (args.options.filter && fs.existsSync(args.options.filter)) {
+	let fsCache = fs.readFileSync(args.options.filter);
+	let cacheObj = JSON.parse(fsCache.toString());
+	filter = (cacheObj.title)? cacheObj : false;
+	console.log(filter);
+}
 
 var epubInfo = {
 	lang: "zh",
@@ -83,24 +96,35 @@ fetch(args.options.url).then(res => res.text())
 	var links = dom("#chapterList a");
 
 	links.each((i, n) => {
-
 		let d = dom(n),
 			no = d.data('title');
 		var href = d.attr('href'),
 			title = d.text();
 		if(href.search(/www\.esjzone\.cc/) == -1) return;
+		if(debug) {
+			console.log("oTitle '%s'",title);
+		}
+
+		if(filter.title) {
+			filter.title.forEach((n, i) => {
+				if(!n[0] && !n[1] && !title) return ;
+				let reg = new RegExp(n[0]);
+				title = title.replace(reg, n[1]);
+			});
+		}
+
+		if(!title) return;
+		if(debug) {
+			console.log("NO:%s, Title:'%s'", i, title);
+		}
+
 		if(tw) {
-			title = converter.convertSync(title)
+			title = converter.convertSync(title);
 		}
 		if (args.options.nu && (args.options.nu <= i)) return;
 
-		if(debug) {
-			console.log("Title %s, %s", i, title)
-		}
-
-		epubInfo.content.push({title:title, data: ""});
 		cache.push({t:title, u:href});
-
+		epubInfo.content.push({title:title, data: ""});
 		getChapter.push(fetch(href).then(res => res.text()));
 	});
 
@@ -114,8 +138,8 @@ fetch(args.options.url).then(res => res.text())
 			console.log("new %s, old %s", chapterListHash, cacheObj.hash);
 		}
 
-		rebuild = (!cacheObj 
-			|| !cacheObj.hash 
+		rebuild = (!cacheObj
+			|| !cacheObj.hash
 			|| (cacheObj.hash != chapterListHash))? true : false;
 	}
 
@@ -129,8 +153,15 @@ fetch(args.options.url).then(res => res.text())
 			let pageDom = cheerio.load(body);
 			let title = pageDom('section h2').first().text();
 			let content = pageDom('section div.forum-content').html();
+			if(filter.content) {
+				filter.content.forEach((n, i) => {
+					if(!n[0] && !n[1] && !content) return ;
+					let reg = new RegExp(n[0]);
+					content = content.replace(reg, n[1]);
+				});
+			}
 			if(tw) {
-				content = converter.convertSync(content)	
+				content = converter.convertSync(content)
 			}
 			if(debug) {
 				console.log("Page %s, %s, %s", i, title, content)
